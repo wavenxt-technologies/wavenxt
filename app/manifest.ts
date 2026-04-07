272813d1934f5600d03f2000deb7c6a0019e4361
westernfrog <haaamansingh007@gmail.com>
2026-04-07 14:32:34 +0530

    Updated

--- app/manifest.ts
+++ app/manifest.ts
@@ -16,6 +16,18 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: "any",
         type: "image/x-icon",
       },
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
     ],
   };
 }
